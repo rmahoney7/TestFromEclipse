521cd12e18dddbaf54a641b23cdd03e43cb38231
rmahoney7 <rachelriannamahoney@gmail.com>
2015-07-07 14:05:43 -0500

    testing

---
+++ StrategyDesignPattern/src/Duck.java
@@ -0,0 +1,35 @@
+public abstract class Duck {
+	//two reference variables for behavior 
+	//interface types - all duck classes
+	//will inherit these reference variables
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	
+	public Duck() {
+	}
+	
+	public abstract void display();
+	
+	public void performFly(){
+		flyBehavior.fly();
+	}
+	
+	public void performQuack(){
+		quackBehavior.quack();
+	}
+	
+	public void setFlyBehavior(FlyBehavior fb){
+		flyBehavior = fb;
+	}
+	
+	public void setQuackBehavior(QuackBehavior qb){
+		quackBehavior = qb;
+	}
+	
+	
+	public void swim (){
+		System.out.println("all ducks float, even decoys");
+	}
+	
+	
+}
---
+++ StrategyDesignPattern/src/FlyRocketPowered.java
@@ -0,0 +1,13 @@
+
+public class FlyRocketPowered implements FlyBehavior {
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+
+		System.out.println("i'm flying with a rocket");
+	}
+	
+
+
+}
